963ccf97edd80bb5d11f20f7224b3d9d046cb766
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-08-22 23:45:06 +0530

    fix/ inital load to chart

--- src/component/barGraph/BarChart.jsx
+++ src/component/barGraph/BarChart.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export default function BarChart() {
   const CHART_DATA = [
     { id: "dep-1", name: "Legal", ticketCount: 32, colour: "#3F888F" },
@@ -13,17 +15,28 @@ export default function BarChart() {
     },
     { id: "dep-7", name: "Events", ticketCount: 43, colour: "#E1CC4F" },
   ];
+
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    // Start the animation when the component mounts
+    setIsLoaded(true);
+  }, []);
+
+
   return(
     <>
         <div className={`max-w-[400px] mx-auto mt-[100px] border-l border-b border-l-[#474747] border-b-[#474747}`} style={{aspectRatio:1/1}}>
         <div className="flex items-end justify-around h-full">
-          {CHART_DATA.map((element) => (
+          {CHART_DATA.map((element,index) => (
             <div
               key={element.id}
               style={{
                 width: '50px',
                 height: `${element.ticketCount}%`,  
                 backgroundColor: element.colour,   
+                transform: isLoaded ? 'translateY(0)' : 'translateY(100%)',  // Initial collapsed state
+                transition: `transform 0.8s ease ${index * 0.1}s`,  // Smooth transition with slight delay per bar
               }}
               
             />
